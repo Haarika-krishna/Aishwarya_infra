82621784e40733697fb13eaa3c74a0b2b7088758
Harika-Boini <harika.b@emobomo.com>
2026-03-31 21:22:34 +0530

    commit

--- aishwarya_infra/src/components/upcoming/UpcomingProjectsDetails.jsx
+++ aishwarya_infra/src/components/upcoming/UpcomingProjectsDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import "./UpcomingprojectsDetails.css";
+import "./UpcomingProjectsDetails.css";
 
 import { Link, useNavigate } from "react-router-dom";
 
